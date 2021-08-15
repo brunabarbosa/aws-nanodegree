ab4cb341c9654d5d64634f46202b9989c980c3b3
Bruna de Oliveira Barbosa <bruna.barbosa@opus-software.com.br>
2021-08-15 13:22:54 -0300

    remove credentials

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,8 +1,8 @@
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
-  access_key = "ASIARCIE67S7JQQY5TN3"
-  secret_key = "/v+vOuWRJjgEfmxZ3x7z8KYM0FAGe3097C0PFldm"
-  token = "FwoGZXIvYXdzECEaDBkQzyfl9y8te87WHiK8AU5faxb5Ese2U7zOaKwx4mjzXJ04ZUwrNgpxCSz8pnJwxS2dh8aELO3+N80pUzipYAKalnySEEpyAbiswX3br8+URhHPYrTbCw04ndHMrGLcbfAvhGkryWhadPowXB3iXU1VMlv644z+pioWDgc6i/qx098sgU2WZyRCR4Cos/aONn5ms7gGJQySZjORGld+TLg/ZbwJpJCPgegq7E9NyfymeOIAdVrHhlNr0XRvnrXy4Rs+s8PiGsf223xQKKqe4YgGMi0nga0L3Bk7dMT4JItDHjpxTHZNw1HR/b6xi5NjJalEJnhhNvfx+qr0D311fBs="
+#   access_key = ""
+#   secret_key = ""
+#   token = ""
   region = "us-east-1"
 }
 
